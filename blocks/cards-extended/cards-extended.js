bf0921a98c3e8f6830cfc785f89506a40e9b6308
Fran Sabolić <fsabolic@foi.hr>
2024-03-20 13:25:09 +0100

    x

--- blocks/cards-extended/cards-extended.js
+++ blocks/cards-extended/cards-extended.js
@@ -43,7 +43,7 @@ function constructProduct(product, ul) {
 
   const productPrice = document.createElement('p');
   productPrice.classList.add('product-price');
-  productPrice.textContent = product.price.toString().split('.')[1].length<2?product.price.toString()+"0":product.price;
+  productPrice.textContent = product.price;
 
   productHTML.append(productPrice);  
   
